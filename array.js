f7147560bd179d5ba7f93e1c1866294d4a48ff11
harish-2006-914 <majjiharish82@gmail.com>
2025-05-15 09:55:20 +0530

    create array.js

---
+++ array.js
@@ -0,0 +1,21 @@
+ const a=[1,2,3];
+ a.push(5);
+ console.log(a);
+ a.pop();
+ console.log(a);
+ a.shift();      // front pop
+ console.log(a);
+ a.unshift(0);   // front push
+ console.log(a);
+const b=[10,20];
+const c=[30,40];
+const d=b.concat(c);
+function display(x){
+    console.log(x);
+}
+d.forEach(display)
+
+
+
+
+
